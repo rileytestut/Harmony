3a2fefc9e34f3a24a72995c4d7ba880357041544
Riley Testut <riley@rileytestut.com>
2018-01-16 16:12:32 -0800

    Adds non-Syncable Placeholder test model object

---
+++ HarmonyTests/Sample Data/Model/Placeholder.swift
@@ -0,0 +1,17 @@
+//
+//  Placeholder.swift
+//  HarmonyTests
+//
+//  Created by Riley Testut on 1/11/18.
+//  Copyright © 2018 Riley Testut. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+import Harmony
+
+@objc(Placeholder)
+public class Placeholder: NSManagedObject
+{
+}
