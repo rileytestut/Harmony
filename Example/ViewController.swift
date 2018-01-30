ce1db850fc0253e039f6258e4fc3cc397480b3c5
Riley Testut <riley@rileytestut.com>
2018-01-30 14:38:39 -0800

    Adds Google Drive authentication support to example app

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
+import Harmony_Drive
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        DriveService.shared.clientID = "1075055855134-qilcmemb9e2pngq0i1n0ptpsc0pq43vp.apps.googleusercontent.com"
+        
+        DriveService.shared.authenticateInBackground { (result) in
+            switch result
+            {
+            case .success: print("Background authentication successful")
+            case .failure(let error): print(error.localizedDescription)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
-
+private extension ViewController
+{
+    @IBAction func authenticate(with sender: UIButton)
+    {
+        DriveService.shared.authenticate(withPresentingViewController: self) { (result) in
+            switch result
+            {
+            case .success: print("Authentication successful")
+            case .failure(let error): print(error.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func deauthenticate(with sender: UIButton)
+    {
+        DriveService.shared.deauthenticate { (result) in
+            switch result
+            {
+            case .success: print("Deauthentication successful")
+            case .failure(let error): print(error.localizedDescription)
+            }
+        }
+    }
 }
 
