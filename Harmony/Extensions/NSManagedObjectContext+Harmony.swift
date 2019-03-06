07bcdd3cff57f1e3d8b9bc22fc8ae709c7ae36bd
Riley Testut <riley@rileytestut.com>
2019-03-06 12:24:26 -0800

    Removes redundant validation logic when updating Syncable object
    No longer necessary because we now compare hashes before uploading records.

--- Harmony/Extensions/NSManagedObjectContext+Harmony.swift
+++ Harmony/Extensions/NSManagedObjectContext+Harmony.swift
@@ -7,33 +7,6 @@
 //
 
 import CoreData
-import ObjectiveC
-
-private var contextCacheKey = 0
-
-class ContextCache
-{
-    private let changedKeys = NSMapTable<NSManagedObject, NSSet>.weakToStrongObjects()
-    
-    func changedKeys(for object: NSManagedObject) -> Set<String>?
-    {
-        let changedKeys = self.changedKeys.object(forKey: object) as? Set<String>
-        return changedKeys
-    }
-    
-    func setChangedKeys(_ changedKeys: Set<String>, for object: NSManagedObject)
-    {
-        self.changedKeys.setObject(changedKeys as NSSet, forKey: object)
-    }
-}
-
-extension NSManagedObjectContext
-{
-    var savingCache: ContextCache? {
-        get { return objc_getAssociatedObject(self, &contextCacheKey) as? ContextCache }
-        set { objc_setAssociatedObject(self, &contextCacheKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
-    }
-}
 
 extension NSManagedObjectContext
 {
